49abb100f10d731be9198969353602aae6c4ab93
Rishab Sareen <rishabhsareen@gmail.com>
2015-07-18 17:32:29 -0400

    Initial commit...again

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.triangle import Triangle
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('customer/index.html', )
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
