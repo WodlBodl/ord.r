9cec27233b9c3d282ac619cb3c552d93ceccef09
tmtiuca <teodor.tiuca@gmail.com>
2015-07-22 09:02:45 -0400

    :changing menu, testing webhook

--- backend/menus.py
+++ backend/menus.py
@@ -11,5 +11,5 @@ lunch = [
 dinner = [
 	{'name':'Steak', 'price':'$1.50', 'ingredients':'Flour, Blueberries, Love', 'quantity':0},
 	{'name':'Toast', 'price':'$2.00', 'ingredients':'Grain, Milk, Sugar', 'quantity':0},
-	{'name':'Coffee', 'price':'$1.00', 'ingredients':'Caffeine, Unicorn Tears', 'quantity':0}
+	{'name':'Beer', 'price':'$1.00', 'ingredients':'Caffeine, Unicorn Tears', 'quantity':0}
 ]
