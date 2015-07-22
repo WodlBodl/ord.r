ba866839326ed97c9200a594fd257ed04e67729e
tmtiuca <teodor.tiuca@gmail.com>
2015-07-22 11:30:39 -0400

    Testing Travis CI

--- backend/menus.py
+++ backend/menus.py
@@ -6,7 +6,7 @@ breakfast = [
 lunch = [
 	{'name':'Pasta', 'price':'$1.50', 'ingredients':'Flour, Blueberries, Love', 'quantity':0},
 	{'name':'Hamburger', 'price':'$2.00', 'ingredients':'Grain, Milk, Sugar', 'quantity':0},
-	{'name':'Salad', 'price':'$1.00', 'ingredients':'Caffeine, Unicorn Tears', 'quantity':0}
+	{'name':'Boob', 'price':'$1.00', 'ingredients':'Caffeine, Unicorn Tears', 'quantity':0}
 ]
 dinner = [
 	{'name':'Pork', 'price':'$1.50', 'ingredients':'Flour, Blueberries, Love', 'quantity':0},
